625476963e4d5c1773e5ee997ba00263224cc882
kaolengmian7 <1156172847@qq.com>
2022-08-25 20:21:11 +0800

    [feature]CSV 工具类

---
+++ csv_util/csv_util.go
@@ -0,0 +1,78 @@
+package csv_util
+
+import (
+	"encoding/csv"
+	"fmt"
+	"github.com/pkg/errors"
+	"os"
+	"path"
+	"time"
+)
+
+type CSV struct {
+	data [][]string
+}
+
+func New() *CSV {
+	c := &CSV{}
+	c.data = make([][]string, 0)
+	return c
+}
+
+// 根据坐标返回数据，rowNum == 0, column == 1 则返回坐标为(0,1)的数据
+func (c *CSV) GetByCoordinate(rowNum int, columnNum int) (res string, err error) {
+	if rowNum >= len(c.data) {
+		return "", errors.WithStack(errors.New(fmt.Sprintf("rowNum(%d) invalid! sliceLen(%d)", rowNum, len(c.data))))
+	}
+	if columnNum >= len(c.data[rowNum]) {
+		return "", errors.WithStack(errors.New(fmt.Sprintf("columnNum(%d) invalid! sliceLen(%d)", columnNum, len(c.data[rowNum]))))
+	}
+
+	return c.data[rowNum][columnNum], nil
+}
+
+// 在某一行末尾追加数据
+func (c *CSV) AddToRow(rowNum int, data string) (err error) {
+	if rowNum >= len(c.data) {
+		return errors.WithStack(errors.New(fmt.Sprintf("rowNum(%d) invalid! sliceLen(%d)", rowNum, len(c.data))))
+	}
+
+	c.data[rowNum] = append(c.data[rowNum], data)
+	return
+}
+
+// 读取 CSV 文件
+func (c *CSV) Read(filePath string) (err error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	c.data, err = r.ReadAll()
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// 导出 CSV 文件到当前目录
+func (c *CSV) Export(fileName string) (err error) {
+	fileName = fmt.Sprintf("%s-%s.csv", fileName, time.Now().Format("2006-01-02"))
+
+	savePath := "./"
+	fp, err := os.Create(path.Join(savePath, fileName)) // 创建文件句柄
+	if err != nil {
+		return
+	}
+	defer fp.Close()
+
+	fp.WriteString("\xEF\xBB\xBF") // 写入UTF-8 BOM
+	w := csv.NewWriter(fp)
+	w.WriteAll(c.data)
+	w.Flush()
+
+	return
+}
---
+++ csv_util/csv_util_test.go
@@ -0,0 +1,114 @@
+package csv_util
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestGetByCoordinate(t *testing.T) {
+	testCase := []struct {
+		rowNum    int
+		columnNum int
+		input     [][]string
+		expect    string
+	}{
+		{
+			rowNum:    0,
+			columnNum: 0,
+			input:     [][]string{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}},
+			expect:    "1",
+		},
+		{
+			rowNum:    2,
+			columnNum: 3,
+			input:     [][]string{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}},
+			expect:    "",
+		},
+		{
+			rowNum:    3,
+			columnNum: 2,
+			input:     [][]string{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}},
+			expect:    "",
+		},
+	}
+	csv := New()
+	for index, tc := range testCase {
+		csv.data = tc.input
+		output, err := csv.GetByCoordinate(tc.rowNum, tc.columnNum)
+		assert.Equal(t, output, tc.expect)
+		if index == 0 {
+			assert.Nil(t, err)
+		} else {
+			assert.NotNil(t, err)
+			log.Printf("%+v", err)
+		}
+	}
+}
+
+func TestAddDataToRow(t *testing.T) {
+	content := "test"
+	testCase := []struct {
+		rowNum int
+		input  [][]string
+		expect [][]string
+	}{
+		{
+			rowNum: 0,
+			input:  [][]string{{"1", "1", "1"}, {"2", "2", "2"}, {"3", "3", "3"}},
+			expect: [][]string{{"1", "1", "1", content}, {"2", "2", "2"}, {"3", "3", "3"}},
+		},
+		{
+			rowNum: 3,
+			input:  [][]string{{"1", "1", "1"}, {"2", "2", "2"}, {"3", "3", "3"}},
+			expect: [][]string{{"1", "1", "1"}, {"2", "2", "2"}, {"3", "3", "3"}},
+		},
+	}
+
+	csv := New()
+	for index, tc := range testCase {
+		csv.data = tc.input
+		err := csv.AddToRow(tc.rowNum, content)
+		assert.ElementsMatch(t, csv.data, tc.expect)
+		if index == 0 {
+			assert.Nil(t, err)
+		} else {
+			assert.NotNil(t, err)
+			log.Printf("%+v", err)
+		}
+	}
+}
+
+func TestRead(t *testing.T) {
+	csv := New()
+	err := csv.Read("./test.csv")
+	expect := [][]string{{"1", "2", "中文3"}, {"4", "5", "中文6"}, {"7", "8", "中文9"}}
+	assert.Nil(t, err)
+	assert.ElementsMatch(t, expect, csv.data)
+}
+
+func TestExport(t *testing.T) {
+	csv := New()
+	csv.data = [][]string{{"1", "2", "3", "test"}, {"4", "5", "6", ""}, {"7", "8", "9", ""}}
+	err := csv.Export("test_export")
+	assert.Nil(t, err)
+
+	fp := fmt.Sprintf("%s-%s.csv", "test_export", time.Now().Format("2006-01-02"))
+	log.Println(fp)
+	// 验证文件存在
+	_, err = os.Stat(fp)
+	assert.Nil(t, err)
+	// 验证内容一致
+	expectCsv := New()
+	err = expectCsv.Read(fp)
+	expect := [][]string{{"\ufeff1", "2", "3", "test"}, {"4", "5", "6", ""}, {"7", "8", "9", ""}} // 因为写入了 UTF-8 BOM 所以要加上 \ufeff
+	assert.ElementsMatch(t, expect, expectCsv.data)
+	// 删除测试文件
+	err = os.Remove(fp)
+	if err != nil {
+		panic(fmt.Sprintf("csv_util2 TestExport failed! os.Remove(%s) failed!", fp))
+	}
+}
